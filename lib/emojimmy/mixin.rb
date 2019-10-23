e6651b01b225c78d788d6eb035427980dd56881c
CENTAR Alex <acentar@cosmocoders.fr>
2019-10-23 11:45:14 +0200

    Remove convert to emoji

--- lib/emojimmy/mixin.rb
+++ lib/emojimmy/mixin.rb
@@ -3,15 +3,7 @@ module Emojimmy
     def self.inject_methods(model, attributes)
       attributes.each do |attribute|
         model.class_eval <<-RUBY, __FILE__, __LINE__ + 1
-          # Before saving the record, convert the attribute value
-          before_save do
-            unless respond_to?("#{attribute}=")
-              raise ArgumentError.new('#{model} must respond to #{attribute}= in order for Emojimmy to store emoji characters in it.')
-            end
-
-            self.#{attribute} = Emojimmy.emoji_to_token(self.#{attribute})
-            true
-          end
+          
 
           # When calling the attribute name, convert its value
           def #{attribute}
